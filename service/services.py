3b18024c4d42047feec39fb121ba367336c8cd3e
Pawan Kumar <83778936+pnkr01@users.noreply.github.com>
2023-08-25 23:49:49 +0530

    Update: services.py

--- service/services.py
+++ service/services.py
@@ -1,5 +1,5 @@
 import boto3
 def aws_config():
-    return boto3.session.Session(profile_name="pawan")
+    return boto3.session.Session(profile_name="demo")
 def text_extract_client(aws_mang_config):
     return aws_mang_config.client(service_name='textract', region_name='us-east-1')
